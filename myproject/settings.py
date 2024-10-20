fded54b5e6754e7f55d30e07fe750acf2efe506f
Raheem047 <126913133+Raheem047@users.noreply.github.com>
2024-10-20 20:18:40 +0530

    Added CSRF trusted origins for Railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8h-$0l$&$ba*yyzc6xre=6!37gq!1jvh-4%1+3xue49p#fso9i'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['djangotest-production-a9ac.up.railway.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://djangotest-production-a9ac.up.railway.app']
+
 
 
 
@@ -124,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
